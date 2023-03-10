66a3e711fc9adccfaae966829b21153206a613f4
Arun74123 <123165450+Arun74123@users.noreply.github.com>
2023-03-10 18:08:07 +0530

    Create Football.sql

---
+++ Football.sql
@@ -0,0 +1,35 @@
+create user football identified by play;
+grant all privileges to football;
+grant create session to football;
+grant create table to football;
+
+create table FootballVenue(
+venue_id int not null primary key,
+venue_name varchar(30) not null,
+city_id int not null,
+capacity int not null
+);
+
+insert into FootballVenue values(2004,'Switzerland',104,10000);
+
+insert into FootballVenue values(2005,'Sweden',105,10000);
+
+insert into FootballVenue values(2006,'West Germany',106,8000);
+
+insert into FootballVenue values(2007,'Mexico',107,10000);
+
+insert into FootballVenue values(2008,'South Africa',108,11000);
+
+insert into FootballVenue values(2009,'Russia',109,18000);
+
+insert into FootballVenue values(2010,'Canada',120,17000);
+
+
+select * from footballvenue;
+
+select count(venue_name)  from footballvenue;
+
+select  venue_name as Location , capacity as Volume from footballvenue;
+
+delete from  footballvenue where venue_name='Russia';
+
